fd6e63c7956e5e9ca6c60e4aec3440ff34bb325b
simofanna <simofanna93@gmail.com>
2018-04-05 16:00:31 +0200

    main structure added

---
+++ src/main/java/com/controller/RouteController.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class RouteController {
+
+
+    @GetMapping(value = "/")
+    public String indexPage(){
+        return "index";
+
+
+    }
+}
